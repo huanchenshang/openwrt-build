a41fb336972fcfc6639f0d46df75615683efd70c
huanchenshang <840943839@qq.com>
2025-08-09 15:44:39 +0800

    Update Settings.sh

--- scripts/Settings.sh
+++ scripts/Settings.sh
@@ -21,3 +21,47 @@ if [[ $TARGET == *"ipq"* ]]; then
 
 	echo "nss version has fixed!"	
 fi
+
+# 添加内核配置以支持 dae
+cat_kernel_config() {
+conde_file="$GITHUB_WORKSPACE/openwrt/target/linux/qualcommax/ipq60xx/config-default"
+  if [ -f "$conde_file" ]; then
+    cat >> "$conde_file" <<EOF
+CONFIG_BPF=y
+CONFIG_BPF_SYSCALL=y
+CONFIG_BPF_JIT=y
+CONFIG_CGROUPS=y
+CONFIG_KPROBES=y
+CONFIG_NET_INGRESS=y
+CONFIG_NET_EGRESS=y
+CONFIG_NET_SCH_INGRESS=m
+CONFIG_NET_CLS_BPF=m
+CONFIG_NET_CLS_ACT=y
+CONFIG_BPF_STREAM_PARSER=y
+CONFIG_DEBUG_INFO=y
+# CONFIG_DEBUG_INFO_REDUCED is not set
+CONFIG_DEBUG_INFO_BTF=y
+CONFIG_KPROBE_EVENTS=y
+CONFIG_BPF_EVENTS=y
+EOF
+    echo "cat_kernel_config to "$conde_file" done"
+  fi
+}
+
+# 修改内核大小
+set_kernel_size() {
+  image_file="$GITHUB_WORKSPACE/openwrt/target/linux/qualcommax/image/ipq60xx.mk"
+  if [ -f "$image_file" ]; then
+    sed -i "/^define Device\/jdcloud_re-ss-01/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" "$image_file"
+    sed -i "/^define Device\/jdcloud_re-cs-02/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+    sed -i "/^define Device\/jdcloud_re-cs-07/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+    sed -i "/^define Device\/redmi_ax5-jdcloud/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+    sed -i "/^define Device\/linksys_mr/,/^endef/ { /KERNEL_SIZE := 8192k/s//KERNEL_SIZE := 12288k/ }" $image_file
+    echo "Kernel size updated in $image_file"
+  else
+    echo "Image file $image_file not found, skipping kernel size update"
+  fi
+}
+
+cat_kernel_config
+set_kernel_size
